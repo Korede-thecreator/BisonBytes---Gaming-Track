247e63a930da1b0c5fde725e56529177103e4c61
CoderObi <123585457+CoderObi@users.noreply.github.com>
2024-03-31 11:03:43 -0400

    Update debounce.v

--- debounce.v
+++ debounce.v
@@ -6,23 +6,26 @@ module debounce(
     output reg btn_out  // Debounced button output
 );
 
-reg [15:0] counter;  // Counter for debounce time
+reg [15:0] score;  // Counter for debounce time
 reg btn_state;  // Current stable state of the button
 
+initial begin
+    score=1'b1;
+end
 always @(posedge clk) begin
     if (btn_in == btn_state) begin
-        // Reset counter if button state is stable
-        counter <= 0;
-    end else begin
-        // Increment counter if button state is changing
-        counter <= counter + 1;
-        // Update button state and output if counter exceeds threshold
-        if (counter >= 16'b1111111111111111) begin
+        score <= score + 1;
+        if (score >= 16'hffff) begin
             btn_state <= btn_in;
             btn_out <= btn_in;
             counter <= 0;
         end
+    end else begin
+        score <= score - 1;
+        if (score == 1'b0) begin
+            counter <= 0;
+            $finish();
+        end
     end
 end
-
 endmodule
